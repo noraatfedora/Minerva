2c4f09ec848a4f87b42ce9005f85b907c3859611
abajajsingh <you@example.com>
2020-04-04 17:26:24 -0700

    includes the new register fields

--- src/auth.py
+++ src/auth.py
@@ -37,7 +37,7 @@ def login():
         
         flash(error)
     
-    return render_template('auth/login.html')
+    return render_template('auth/login.html', title = "Log In")
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
@@ -74,7 +74,7 @@ def register():
             return redirect(url_for('auth.login'))
         
         flash(error)
-    return render_template('auth/register.html')
+    return render_template('auth/register.html', title = 'Register')
 
 @bp.before_app_request
 def load_logged_in_user():
