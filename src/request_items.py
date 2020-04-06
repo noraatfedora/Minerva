cd63ae3db7e508b62e758042f3688ec95f252742
Jared Goodman <jaredgoodman03@gmail.com>
2020-04-06 13:14:39 -0700

    Remove dumb tea bag thing

--- src/request_items.py
+++ src/request_items.py
@@ -15,24 +15,21 @@ bp = Blueprint('request_items', __name__)
 @bp.route('/request_items', methods=('GET', 'POST'))
 @login_required
 def request_items():
+    print("asdfdsfdsfljkfsdljk")
     if request.method == "POST":
-        items = request.form['items']
-        error = None
-        print("Items: " + items)
-        if not items:
-            error = "Items are required."
-        
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            sqlcommand = """UPDATE user SET items = ? WHERE email = ?"""
-            print(sqlcommand)
-            db.execute(
-                sqlcommand,
-                (items, g.user['email'])
-            )
-            db.commit()
-            return redirect("/index")
+        print("in if statement")
+        db = get_db()
+        sqlcommand = "UPDATE user"
+        for item in itemsList.values():
+            name = request.form[item['name']]
+            sqlcommand += " SET " + name + "= ?"
+        sqlcommand += "WHERE email = ?"
+        print(sqlcommand)
+        db.execute(
+            sqlcommand,
+            tuple(itemsList.values()) + (g.user['email'],)
+        )
+        db.commit()
+        return redirect("/home")
     
     return render_template("request_items.html",items = itemsList.values())
