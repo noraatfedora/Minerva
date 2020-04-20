6ed4208afbedfa16b17835a578130f8ba1e0bf1c
Jared Goodman <jaredgoodman03@gmail.com>
2020-04-19 18:11:05 -0700

    Convert auth.py to new library

--- src/application.py
+++ src/application.py
@@ -3,13 +3,12 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for, Flask
 )
 
-
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'requests.sqlite'),
+        SECRET_KEY = 'dev',
+        SQLALCHEMY_DATABASE_URI = 'sqlite://instance/requests.sqlite'
     )
 
     if test_config is None:
--- src/db.py
+++ src/db.py
@@ -1,19 +1,29 @@
-import sqlite3
 import click
 import json
 from flask import current_app, g, Flask
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
 from flask.cli import with_appcontext
 
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES,
-            timeout=10
-        )
-        g.db.row_factory = sqlite3.Row
+engine = create_engine('sqlite:///instance/requests.sqlite?check_same_thread=False')
+meta = MetaData()
+users = Table(
+    'users', meta,
+    Column('id', Integer, primary_key = True),
+    Column('email', String),
+    Column('password', String),
+    Column('role', String),
+    Column('cellPhone', String),
+    Column('instructions', String),
+    Column('homePhone', String),
+    Column('address', String),
+    Column('items', String), # Stored as a JSON because g.db doesn't support adding Columns
+    Column('completed', Integer) # either 0 or 1
+)
+conn = engine.connect()
 
-    return g.db
+# TODO: Remove this 
+def get_db():
+    return conn 
 
 def close_db(e=None):
     db = g.pop('db', None)
@@ -21,44 +31,7 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
-def init_db():
-    db = get_db()
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
-    
-
-
-@click.command('init-db')
-@with_appcontext
-def init_db_command():
-    init_db()
-    update_db()
-    click.echo('Initialized the database.')
-
-@click.command('update-db')
-@with_appcontext
-def update_db_command():
-    update_db()
-    click.echo('Updated the database.')
-    
-def update_db():
-    db = get_db()
-    # take the contents of items.json
-    itemsRaw = open("src/items.json").read()
-    values = json.loads(itemsRaw).values()
-    print("Values: " + str(values))
-    # Add these items as attiributes to a user
-    for item in values:
-        name = item['name']
-        try:
-            print("Name: " + name)
-            db.execute("ALTER TABLE user ADD COLUMN ? INTEGER " "DEFAULT 0", (name,))
-        except:
-            print(name + " already exists in the database...")
-    db.commit()
-
 def init_app(app):
     app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
-    app.cli.add_command(update_db_command)
 
+    meta.create_all(engine)
