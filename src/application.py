7cd489e20cdd110e066cdbb1f350012adb3177ac
Jared Goodman <jaredgoodman03@gmail.com>
2020-04-21 12:30:24 -0700

    fix items.json and request page again
    it keeps getting deleted because I don't know how to use git

--- src/application.py
+++ src/application.py
@@ -8,7 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY = 'dev',
-        SQLALCHEMY_DATABASE_URI = 'sqlite://instance/requests.sqlite'
+        #SQLALCHEMY_DATABASE_URI = 'sqlite://instance/requests.sqlite'
     )
 
     if test_config is None:
