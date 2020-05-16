d733a4def8b4e8e03823033d1b70d6bfe14fa67a
Jared Goodman <jaredgoodman03@gmail.com>
2020-05-15 21:50:28 -0700

    Add the logo

--- src/auth.py
+++ src/auth.py
@@ -3,7 +3,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
-from db import users, conn, meta
+from db import users, conn
 from sqlalchemy import select, update 
 from json import loads
 
