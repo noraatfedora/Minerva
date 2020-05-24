c89a19743328dbdb841fab3a0d0a9a9b37937960
Jared Goodman <jaredgoodman03@gmail.com>
2020-05-24 00:16:06 -0700

    Add google distance matrix function

---
+++ src/google_api.py
@@ -0,0 +1,28 @@
+import requests
+from json import  loads
+
+def matrix(origins, destinations):
+    # Example URL:
+    #https://maps.googleapis.com/maps/api/distancematrix/json?origins=Boston,MA|Charlestown,MA&destinations=Lexington,MA|Concord,MA&departure_time=now&key=YOUR_API_KEY
+    requestString = ("https://maps.googleapis.com/maps/api/distancematrix/json?origins="
+        + addAdresses(origins)
+        + "&destinations="
+        + addAdresses(destinations)
+        + "&departure_time=now"
+        + "&key="
+        + open('GOOGLE_API_KEY', 'r').read())
+
+    return loads(requests.get(requestString).content)
+
+def addAdresses(addresses):
+    toReturn = ''
+    for address in addresses:
+        # security
+        address.replace('&', '')
+        address.replace('|', '')
+        
+        toReturn += address + "|" 
+
+    # Remove the last '|'
+    return toReturn[:-1]
+
