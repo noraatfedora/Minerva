8e169697f0d7c11c00a750e092f80007feb9fecc
jaredgoodman03 <42077013+jaredgoodman03@users.noreply.github.com>
2020-06-06 22:54:29 -0700

    Delete set_environment_variables.py

--- src/set_environment_variables.py
+++
@@ -1,5 +0,0 @@
-from os import environ
-environ['EMAIL_PASSWORD'] = 'UuR3W5KbBVSuyQU'
-environ['EMAIL_SENDER'] ='contactusminerva@gmail.com'
-environ['GOOGLE_API'] ='AIzaSyA2HTf7P3pUaSbU2is--TH7CZ8rr8JTN54'
-environ['INSTANCE_PATH'] ='/../instance/'
