82c00a7ccc1e4b350fc725903f3c655e25d0c542
Jared Goodman <jaredgoodman03@gmail.com>
2020-09-23 21:42:00 -0700

    Add basic claiming of orders by volunteers

--- minerva/backend/routes/dashboard.py
+++ minerva/backend/routes/dashboard.py
@@ -5,9 +5,10 @@ from werkzeug.exceptions import abort
 from minerva.backend.routes.auth import login_required, volunteer_required
 from json import loads, dumps
 from db import users, conn, orders, items
+from assign import getNextRoute
 from sqlalchemy import and_, select
 from minerva.backend.apis.email import send_recieved_notification
-from datetime import date
+from datetime import date, datetime, timedelta
 from os import environ
 import google_maps_qr, pdfkit
 
@@ -21,25 +22,33 @@ bp = Blueprint('dashboard', __name__)
 def dashboard():
     itemsList = conn.execute(items.select(items.c.foodBankId==g.user.foodBankId)).fetchall()
 
-    ordersDict = getOrders(g.user.id)
-    if request.method == "GET" and "checkin" in request.args.keys():
-        conn.execute(users.update().where(users.c.id==g.user.id).values(checkedIn=str(date.today())))
+    userList = getUsers()
+    if request.method == "GET" and "claimRoute" in request.args.keys():
+        route = getNextRoute(g.user.foodBankId)
+        print("route: " + str(route))
+        conn.execute(users.update().where(users.c.id==g.user.id).values(ordering=dumps(route)))
         return redirect('/dashboard')
     if request.method == "POST":
         firstKey = next(request.form.keys())
-        orderId = int(firstKey)
-        query = select([orders.c.completed]).where(orders.c.id==orderId)
-        completed = conn.execute(query).fetchone()[0]
+        userId = int(firstKey)
+        conn.execute(
+            users.update().where(
+                users.c.id==userId
+            ).values(lastDelivered=datetime.now())
+        )
         # If you refresh the page and resend data, it'll send 2 confirmation emails. This if statement prevents that.
-        if (completed == 0):
-            email = ordersDict[orderId]['email']
-            send_recieved_notification(email)
-            conn.execute(orders.update().where(orders.c.id==orderId).values(completed=1))
-            ordersDict = getOrders(g.user.id)
+        email = conn.execute(
+            select([users.c.email]).where(
+                users.c.id==userId
+            )
+        )
+        email = email
+        send_recieved_notification(email)
+        userList = getUsers()
 
     checkedIn = g.user.checkedIn == str(date.today())
     print("ordering: " + str(g.user.ordering))
-    return render_template("dashboard.html", orders=ordersDict, items=itemsList, google_maps = google_maps_qr.make_url(loads(g.user.ordering)), checkedIn=checkedIn)
+    return render_template("dashboard.html", orders=userList, items=[], google_maps = google_maps_qr.make_url([]), checkedIn=checkedIn)
 
 @login_required
 @volunteer_required
@@ -56,7 +65,7 @@ def qr_mark_as_complete():
 
 @bp.route('/driver_printout', methods=('GET', 'POST'))
 def driver_printout():
-    ordersDict =getOrders(g.user.id)
+    ordersDict =getUsers(g.user.id)
 
     if request.method == "POST":
         orderId = int(next(request.form.keys()))
@@ -67,10 +76,10 @@ def driver_printout():
             email = ordersDict[orderId]['email']
             send_recieved_notification(email)
             conn.execute(orders.update().where(orders.c.id==orderId).values(completed=1))
-            ordersDict = getOrders(g.user.id)
-    
+            ordersDict = getUsers(g.user.id)
+
     html = render_template("driver_printout.html", orders=ordersDict, volunteer=g.user)
-    
+
     pdf = pdfkit.from_string(html, False)
 
     response = make_response(pdf)
@@ -79,30 +88,23 @@ def driver_printout():
 
     return response
 
-# Returns a dictionary where the keys are the order ID's,
-# and the values are dicts with attributes about that order (contents, email, etc.)
-# All of these should be uncompleted orders, since an order is removed from a volunteer's
-# list when it's completed.
-def getOrders(volunteerId):
+# Returns a list of users based off the volunteer's ordering column
+def getUsers():
+    row2dict = lambda r: {c.name: str(getattr(r, c.name)) for c in users.columns}
     # Get the ID's that our volunteer is assigned to
-    orderList = conn.execute(orders.select(and_(orders.c.volunteerId==g.user.id, orders.c.completed==0, orders.c.bagged==1))).fetchall()
+    ordering = loads(g.user.ordering)
+    toReturn = []
+    for userId in ordering:
+        if userId != g.user.foodBankId: # Stupid to put the food bank on the user's list of orders
+            toReturn.append(
+                row2dict(
+                    conn.execute(
+                        users.select().where(users.c.id==userId)
+                    ).fetchone()
+                )
+            )
 
-    toReturn = {} # We'll return this later
-    for order in orderList:
-        toReturn[order.id] = {}
-        user = conn.execute(users.select().where(users.c.id==order['userId'])).fetchone()
-        # Add all our user's attributes to our order
-        userColumns = conn.execute(users.select()).keys()
-        userColumns.remove('id')
-        for column in userColumns:
-            toReturn[order.id][str(column)] = str(getattr(user, str(column)))
-        # And all our order's attributes
-        for column in conn.execute(orders.select()).keys():        
-            toReturn[order.id][str(column)] = str(getattr(order, str(column)))
-
-        toReturn[order.id]['contents'] = loads(toReturn[order.id]['contents'])
-
-    print(toReturn)
+    print("Users: " + str(toReturn))
     return toReturn
 
 def getAddresses(orders):
