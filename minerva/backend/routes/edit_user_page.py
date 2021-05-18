763c20e35e505eb65324af72c4a2346879f1e59d
Jared Goodman <jaredgoodman03@gmail.com>
2021-05-18 16:35:08 -0700

    Update edit users page

--- minerva/backend/routes/edit_user_page.py
+++ minerva/backend/routes/edit_user_page.py
@@ -1,16 +1,17 @@
 from os import environ
-from flask import ( Blueprint, flash, g, redirect, render_template,
-    request, session, url_for, Flask, make_response, send_file
-)
+from flask import (Blueprint, flash, g, redirect, render_template,
+                   request, session, url_for, Flask, make_response, send_file
+                   )
 import geopy
-from minerva.backend.routes.auth import login_required, admin_required 
+from minerva.backend.routes.auth import login_required, admin_required
 from minerva.backend.apis.db import users, conn, routes
 
 bp = Blueprint('edit_user_page', __name__)
 
+
 @bp.route('/edit_user/<userId>', methods=['GET', 'POST'])
 def edit_users(userId):
-    user = conn.execute(users.select().where(users.c.id==userId)).fetchone()
+    user = conn.execute(users.select().where(users.c.id == userId)).fetchone()
     if request.method == "GET":
         return render_template('edit_user.html', user=user)
     else:
@@ -22,17 +23,22 @@ def edit_users(userId):
         if address != user['formattedAddress']:
             API_KEY = environ['GOOGLE_API']
             googleWrapper = geopy.geocoders.GoogleV3(api_key=API_KEY)
-            coords = googleWrapper.geocode(query=request.form['address'], timeout=100)
+            coords = googleWrapper.geocode(
+                query=request.form['address'], timeout=100)
             address = coords[0]
             latitude = coords[1][0]
             longitude = coords[1][1]
 
-        conn.execute(users.update().where(users.c.id==userId).values(
-        formattedAddress = address,
-        address2 = request.form['address2'],
-        email = request.form['email'],
-        cellPhone = request.form['cellPhone'],
-        instructions = request.form['instructions'],
-        latitude = latitude,
-        longitude = longitude))
+        disabled = user['disabled'] or 'disable' == request.form['action-on-exit']
+
+        conn.execute(users.update().where(users.c.id == userId).values(
+            name=request.form['name'],
+            formattedAddress=address,
+            address2=request.form['address2'],
+            email=request.form['email'],
+            cellPhone=request.form['cellPhone'],
+            instructions=request.form['instructions'],
+            latitude=latitude,
+            longitude=longitude,
+            disabled=disabled))
         return redirect('/all_users#card-' + str(user['id']))
